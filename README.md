d7a7d509875a0e0d97fd1f9440b0342c40d73bf9
nchen0 <nicky.chen0@gmail.com>
2020-01-27 14:25:19 -0500

    add linting & fixed up some optional parameters

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
+# Steps:
+
+- To start server: `npm start`, open up an API tool like Postman, and open up a GET on `localhost:8000/places`. Add request bodies to return non-zero results.
+- To test: `npm test`.
+- To run lint: `npm run lint`.
+
 # Endpoints
+
 ## GET /places
-- Parameters are taken through the request body, example shown below. 
+
+- Parameters are taken through the request body, example shown below.
 - Parameters taken: latitude, longitude, customerName, type, numberOfLocations, outputType, language
 - Required parameters: latitude, longitude, and customerName
 - Optional parameters: type (default "all"), numberOfLocations (default 20), outputType (default "json"), language (default "en")
-
-## Steps:
-- To run: `npm start`, open up an API tool like Postman, and open up a GET on `localhost:8000/places`. Add request bodies to return non-zero results.
-- To test: `npm test`. 
--- index.js
+++ index.js
@@ -7,23 +7,32 @@ server.use(express.json());
 const { validateInput, languages } = require("./helpers/helper-functions");
 
 server.get("/places", (req, res) => {
-  const { latitude, longitude, customerName } = req.body;
+  let { latitude, longitude, customerName } = req.body;
+
+  // Validating required parameters are there
   const validatedSearch = validateInput(req);
   if (validatedSearch.error) {
     return res.status(400).send(validatedSearch.error.details[0].message);
   }
+
+  // Optional parameters
   const language = languages[req.body] || "en";
   const type = req.body.type || "all";
   const outputType = req.body.outputType || "json";
-  // console.log(
-  //   "url: ",
-  //   `https://maps.googleapis.com/maps/api/place/textsearch/${output}?query=${customerName}&location=${latitude},${longitude}&type=${type}&language=${language}&radius=10000&language=spanish&key=${process.env.API_KEY}`
-  // );
+  const number = req.body.number || 20;
+
+  // If the customer name has more than one word, Google API needs it to include +'s between them.
+  if (customerName.indexOf(" ") >= 0) {
+    customerName = customerName.replace(/\s/g, "+");
+  }
   axios
     .get(
-      `https://maps.googleapis.com/maps/api/place/textsearch/${outputType}?query=${customerName}&location=${latitude},${longitude}&type=${type}&language=${language}&radius=10000&language=spanish&key=${process.env.API_KEY}`
+      `https://maps.googleapis.com/maps/api/place/textsearch/${outputType}?query=${customerName}&pagetoken=1&location=${latitude},${longitude}&type=${type}&language=${language}&radius=10000&language=spanish&key=${process.env.API_KEY}`
     )
     .then(response => {
+      if (number < 20) {
+        return res.send(response.data.splice(0, number));
+      }
       res.send(response.data);
     })
     .catch(err => {
